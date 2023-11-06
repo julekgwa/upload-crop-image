719e8b71c3f9f8ff46b4d5d5f67820a8243c9d56
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2023-11-06 09:21:11 +0200

    validate text against the cropped area

--- src/app/common/components/CropperModal.tsx
+++ src/app/common/components/CropperModal.tsx
@@ -1,9 +1,10 @@
-import { Button, Dialog, DialogBody, DialogFooter } from '@material-tailwind/react';
+import { Button, Dialog, DialogBody, DialogFooter, Input, Spinner } from '@material-tailwind/react';
 import React from 'react';
 import type { Area, Point } from 'react-easy-crop';
 import Cropper from 'react-easy-crop';
 import { BsZoomIn, BsZoomOut  } from 'react-icons/bs';
 import { AiOutlineRotateLeft, AiOutlineRotateRight } from 'react-icons/ai';
+import Tesseract from 'tesseract.js';
 
 type Props = {
   show: boolean;
@@ -15,17 +16,130 @@ const MAX_ZOOM = 3;
 const ZOOM_STEP = 0.1;
 const MAX_ROTATION = 360;
 
+function degreesToRadians(degrees: number): number {
+
+  return (degrees * Math.PI) / 180;
+
+}
+
+function rotateSize(width: number, height: number, rotation: number) {
+
+  const rotRad = degreesToRadians(rotation);
+
+  return {
+    boxWidth:
+      Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+    boxHeight:
+      Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+  };
+
+}
+
 export function CropperModal({ show, onClose, source }: Props) {
 
+  const desiredWidth = 600;
+  const desiredHeight =600;
   const [crop, setCrop] = React.useState<Point>({ x: 0, y: 0 });
   const [zoom, setZoom] = React.useState(1);
   const [rotation, setRotation] = React.useState(0);
-  const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
+  const [isValidId, setIsValidId] = React.useState<boolean>();
+  const [canvasData, setCanvasData] = React.useState<HTMLCanvasElement | null>(null);
+  const [idNumber, setIdNumber] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+  const validateID = async() => {
 
-    console.log(croppedArea, croppedAreaPixels);
+    try {
+
+      setIsValidId(undefined);
+      setIsLoading(true);
+      const { data: { text } } = await Tesseract.recognize(canvasData, 'eng', {
+        // tessedit_char_whitelist: '0123456789', // Recognize only digits (for South African ID)
+      });
+
+      setIsValidId(text.trim().includes(idNumber));
+
+    } catch (error) {
+
+      console.log(error);
+
+    } finally {
+
+      setIsLoading(false);
+
+    }
 
   };
 
+  const onCropComplete = (_: Area, croppedAreaPixels: Area) => {
+
+    try {
+
+      if (!croppedAreaPixels) return;
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      if (!ctx) return;
+
+      const image = new Image();
+
+      image.crossOrigin = 'anonymous';
+
+      image.onload = () => {
+
+        // set the size for canvas
+
+        const { boxWidth, boxHeight } = rotateSize(
+          image.width,
+          image.height,
+          rotation
+        );
+
+        const rotRad = degreesToRadians(rotation);
+
+        canvas.width = boxWidth;
+        canvas.height = boxHeight;
+
+        ctx.translate(boxWidth / 2, boxHeight / 2);
+        ctx.rotate(rotRad);
+        ctx.translate(-image.width / 2, -image.height / 2);
+
+        // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        ctx.drawImage(image, 0, 0);
+
+        const croppedCanvas = document.createElement('canvas');
+        const croppedCtx = croppedCanvas.getContext('2d');
+
+        if (!croppedCtx) return;
+
+        const { x, y, width, height } = croppedAreaPixels;
+
+        croppedCanvas.width = desiredWidth;
+        croppedCanvas.height = desiredHeight;
+        croppedCtx.drawImage(
+          canvas,
+          x,
+          y,
+          width,
+          height,
+          0,
+          0,
+          desiredWidth,
+          desiredHeight
+        );
+
+        setCanvasData(croppedCanvas);
+
+      };
+
+      image.src = source;
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  };
   const handleOnRotation = React.useCallback((rotationValue: number) => {
 
     setRotation(rotationValue);
@@ -35,7 +149,7 @@ export function CropperModal({ show, onClose, source }: Props) {
   return (
     <Dialog open={show} handler={onClose}>
       <DialogBody className='h-[300px] md:h-[600px] p-5'>
-        <div className='w-80 bg-black'>
+        <div className='bg-black w-80'>
           <Cropper
             image={source}
             crop={crop}
@@ -49,9 +163,14 @@ export function CropperModal({ show, onClose, source }: Props) {
           />
         </div>
       </DialogBody>
-      <DialogFooter className='flex items-center justify-evenly'>
+      <DialogFooter className='flex flex-col items-start'>
 
-        <div className='flex w-full gap-4 items-center justify-end'>
+        <div className='flex items-center gap-4 justify-evenly '>
+          <Input crossOrigin={false} label='ID number' onChange={e => setIdNumber((e.target.value))} />
+          {isValidId === undefined ? '' : isValidId ? <p className='font-bold text-green-600 !w-20'>Valid ID</p> : <p className='!w-24 text-red-600 font-bold'>Invalid ID</p>}
+        </div>
+
+        <div className='flex items-center justify-end w-full gap-4'>
           <AiOutlineRotateLeft onClick={() => {
 
             if (rotation > 0) setRotation(rotation - 1);
@@ -82,8 +201,9 @@ export function CropperModal({ show, onClose, source }: Props) {
           >
             <span>Cancel</span>
           </Button>
-          <Button variant="gradient" color="green" onClick={onClose}>
-            <span>Confirm</span>
+          <Button disabled={!idNumber} className='flex items-center gap-2' variant="gradient" color="green" onClick={validateID}>
+            {isLoading && <Spinner />}
+            <span>Validate</span>
           </Button>
         </div>
 
