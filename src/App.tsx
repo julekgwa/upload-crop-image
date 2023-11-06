b0c8ab8ed0a7d94e738473b81d86e36dd855dd51
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2023-11-06 09:30:00 +0200

    validate text against the cropped area

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <main className='lg:w-[1200px] px-4 md:w-[540px] mx-auto pt-16'>
-      <section className='bg-white flex flex-col gap-7 rounded-md p-12 shadow-md'>
-        <h1 className='text-xl font-bold text-center'>Id Upload</h1>
+      <section className='flex flex-col p-12 bg-white rounded-md shadow-md gap-7'>
+        <h1 className='text-xl font-bold text-center'>Id Upload with Tessaract OCR</h1>
         <div>
           <FileUploader />
         </div>
